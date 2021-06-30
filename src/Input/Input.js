8d4866bc9f2a4571fde5d8d82d93b7b0c884b2ec
shatayu <shatayu@outlook.com>
2021-06-30 01:02:04 -0700

    added clear button

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -58,6 +58,13 @@ export default function Input({onFinalizeEntries}) {
                 </form>
             </div>
             <div className={styles.buttonContainer}>
+            <div className={entries.length > 0 ? styles.button + ' ' + styles.enabledButton : styles.button + ' ' + styles.disabledButton} onClick={
+                () => {
+                    setEntries([]);
+                }
+            }>
+                <svg width="24" height="24" viewBox="0 0 24 24"><path d="M9 19c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5-17v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712zm-3 4v16h-14v-16h-2v18h18v-18h-2z"/></svg>
+            </div>
             <div className={entries.length > 1 ? styles.button + ' ' + styles.enabledButton : styles.button + ' ' + styles.disabledButton} onClick={async () => {
                     // generate shared link
                     const url = 'https://3ocshrauf1.execute-api.us-west-1.amazonaws.com/lists';
@@ -68,10 +75,8 @@ export default function Input({onFinalizeEntries}) {
                     };
 
                     const result = await axios.put(url, body);
-                    console.log(result);
 
                     const baseURL = window.location.href.substr(0, window.location.href.lastIndexOf('/')) + '/';
-                    console.log(baseURL);
                     copy(baseURL + result.data.new_id);
                 }}><svg width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg></div>
                 <div
