75979edf7a5dabd77d4dd3dcaaa6bccaf16b7ca0
shatayu <shatayu@outlook.com>
2021-08-04 01:22:19 -0700

    modified clear all button to accomodate title

--- src/Input/Buttons/DeleteAllEntriesButton.react.js
+++ src/Input/Buttons/DeleteAllEntriesButton.react.js
@@ -1,19 +1,22 @@
 import GenericButton from './GenericButton';
 import { ReactComponent as Trash } from '../../assets/trash.svg'
 import { useRecoilState } from 'recoil';
-import { EntriesListAtom } from '../../atoms';
+import { EntriesListAtom, TitleAtom } from '../../atoms';
 import styles from '../Input.module.css';
 
 export default function DeleteAllEntriesButton() {
     const [entriesList, setEntriesList] = useRecoilState(EntriesListAtom);
+    const [title, setTitle] = useRecoilState(TitleAtom);
     return (
         <GenericButton
             icon={<Trash className={styles.buttonIcon} />}
             text='CLEAR ALL'
-            isEnabled={entriesList.length > 0}
-            onClick={() => setEntriesList([])}
+            isEnabled={entriesList.length > 0 || title.length}
+            onClick={() => {
+                setEntriesList([]);
+                setTitle('');
+            }}
             isDeleteButton={true}
-
         />
     );
 }
